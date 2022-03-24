94501b0c7c10a58723c3d4ecf4fd91cabbf13625
Eric Treadway <eric.s.treadway@gmail.com>
2022-03-24 10:31:19 -0500

    day and year calc is working

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(561600);
+    }
+}
---
+++ src/MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,17 @@
+public class MinutesToYearsAndDaysCalculator {
+    public static void printYearsAndDays(long minutes){
+        if (minutes < 0 ){
+            System.out.println("Invalid Value");
+        }else {
+            long hours = minutes / 60;
+            long days = hours / 24;
+            long years = days / 365;
+            days = days % 365;
+
+            System.out.println(minutes + " min = " +
+                    years + " y and " +
+                    days + " d");
+        }
+
+    }
+}
